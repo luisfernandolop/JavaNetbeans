b4595cf9e54d77143f17522c41ea4b46adf84d09
Tu Nombre <luisfernandolop12@gmail.com>
2023-08-23 20:34:57 +0100

    Carga Inicial

---
+++ Documents/NetBeansProjects/JavaNetbeans/src/main/java/com/mycompany/javanetbeans/JavaNetbeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javanetbeans;
+
+/**
+ *
+ * @author Almuno
+ */
+public class JavaNetbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
